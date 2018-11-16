f953fba0576251d65b3e3fc81b874b1549ec1ec2
ilin chang <niliyzhang@foxmail.com>
2018-11-16 11:19:42 +0800

    static city'page

---
+++ src/pages/city/City.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<city-header/>
+		<city-search/>
+		<city-list/>
+	</div>
+</template>
+<script>
+import cityHeader from './components/header'
+import citySearch from './components/search'
+import cityList from './components/list'
+export default {
+	name:'',
+	components:{
+		cityHeader,
+		citySearch,
+		cityList
+	}
+
+}
+
+</script>
+<style lang="stylus" scroped></style>
