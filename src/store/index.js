8397bd65b55b0f8315069511e580a44606fcf329
ilin chang <niliyzhang@foxmail.com>
2018-11-19 14:11:53 +0800

    use localStorage to remain user's city

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,25 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+let defaultCity = '北京'
+try{
+	if(localStorage.city){
+		defaultCity = localStorage.city
+	}
+}catch (e){}
+
+
 export default new Vuex.Store({
 	state:{
-		city:'北京',
+		city:defaultCity,
 	},
 	mutations: {
 	  changeCity (state,city) {
 	    state.city = city
+	    try{
+			localStorage.city = city
+		}catch (e){}
 	  }
 	}
 })
+
