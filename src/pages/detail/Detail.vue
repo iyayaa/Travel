d09a996483411cec8442c538a46733a4a92a79b4
ilin chang <niliyzhang@foxmail.com>
2018-11-20 13:46:53 +0800

    详情页-header渐隐渐现

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,17 +1,21 @@
 <template>
 	<div>
 		<detailBanner/>
+		<detailHeader/>
+		<div class="test">ff</div>	
 	</div>
 </template>
 
 <script>
 import detailBanner from './components/banner'
+import detailHeader from './components/header'
 export default {
 	name:'',
 	props:{
 	},
 	components:{
 		detailBanner,
+		detailHeader,
 	}
 }
 	
@@ -19,5 +23,6 @@ export default {
 
 <style lang="stylus" scoped>
   @import '~styles/mixins.styl'
-
+  .test
+  	height 50rem
 </style>
